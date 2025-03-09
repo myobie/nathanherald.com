4970135b1d9e023ffc5079c882e9f96ffc3f5039
Nathan Herald <me@nathanherald.com>
2025-03-09 21:27:36 +0100

    Remove domain from a lot of links

--- TODO.md
+++ TODO.md
@@ -3,12 +3,12 @@
 - [x] `rm public/links`
 - [x] `rm public/keybase.txt` I don’t need that anymore
 - [x] Implement redirect from `/writing/*` to `/posts/*` and `rm public/writing`
-- [ ] Remove hash from css and js, etags should be good enough (find and replace)
+- [x] Remove hash from css and js, etags should be good enough (find and replace)
 - [ ] Copy all the original markdown files into the folders to sit next to the `index.html`s and then `rm ./content`
     - [ ] `rm ./layouts`
-- [ ] How do I do local links? They are all nathanherald.com after hugo did it’s thing
-- [ ] Is there an automated way to soft wrap the text in long paragraphs in every page?
-- [ ] Why is it `/posts/rss.xml` and not just `/rss.xml`? Move it?
+- [x] How do I do local links? They are all nathanherald.com after hugo did it’s thing
+- [x] Is there an automated way to soft wrap the text in long paragraphs in every page?
+- [x] Why is it `/posts/rss.xml` and not just `/rss.xml`? Move it?
 - [ ] What are the steps to adding a new post?
     - [ ] Write in markdown
     - [ ] Convert to html
--- migrate.exs
+++ migrate.exs
@@ -4,32 +4,10 @@ Path.wildcard("public/**/*.html")
 end)
 |> Enum.map(fn {file, path} ->
   pattern =
-    ~S{<link rel="stylesheet" type="text/css" href="https://nathanherald.com/combined.min.31312963d9b6322f358005bab4cfc1a3a286b0ec67f5df6e22434de51d71c004.css" integrity="sha256-MTEpY9m2Mi81gAW6tM/Bo6KGsOxn9d9uIkNN5R1xwAQ=">}
+    ~r{<a href=(\s*)"https://nathanherald.com/posts/}
 
   replacement =
-    ~S{<link rel="stylesheet" type="text/css" href="/styles.css">}
-
-  content = String.replace(file, pattern, replacement)
-
-  {content, path}
-end)
-|> Enum.map(fn {file, path} ->
-  pattern =
-    ~S{<script async src="https://nathanherald.com/behavior.min.e41a74d07fee42e04abe287c617e80b67998e06c69d3d3c07c6cd91f272bc0bc.js" integrity="sha256-5Bp00H/uQuBKvih8YX6AtnmY4Gxp09PAfGzZHycrwLw="></script>}
-
-  replacement =
-    ~S{<script async src="/behavior.js"></script>}
-
-  content = String.replace(file, pattern, replacement)
-
-  {content, path}
-end)
-|> Enum.map(fn {file, path} ->
-  pattern =
-    ~S{<link rel="alternate" type="application/rss+xml" href="https://nathanherald.com/posts/rss.xml" title="Archive of all the posts on this site on nathanherald.com">}
-
-  replacement =
-    ~S{<link rel="alternate" type="application/rss+xml" href="/rss.xml" title="Feed of all the posts on nathanherald.com">}
+    ~S{<a href="/posts/}
 
   content = String.replace(file, pattern, replacement)
 
