ab3c5c30388b4153f85f14c121139a1da0d51e20
Nathan Herald <me@nathanherald.com>
2025-03-09 21:37:07 +0100

    Fix rss links

--- migrate.exs
+++ migrate.exs
@@ -1,28 +1,15 @@
-Path.wildcard("content/**/*.md")
+Path.wildcard("public/**/*.html")
 |> Enum.map(fn path ->
   {File.read!(path), path}
 end)
 |> Enum.map(fn {file, path} ->
-  pattern = ~r{^content/}
+  pattern = ~S{<a href="/posts/rss.xml">}
 
-  replacement = "public/"
+  replacement = ~S{<a href="/rss.xml">}
 
-  path = String.replace(path, pattern, replacement)
+  file = String.replace(file, pattern, replacement)
 
   {file, path}
-end)
-|> Enum.map(fn {file, path} ->
-  if String.ends_with?(path, "index.md") do
-    {file, path}
-  else
-    pattern = ~r{/(.+)\.md$}
-
-    replacement = ~S{/\1/index.md}
-
-    path = String.replace(path, pattern, replacement)
-
-    {file, path}
-  end
 end)
 |> Enum.map(fn {file, path} ->
   File.write!(path, file)
