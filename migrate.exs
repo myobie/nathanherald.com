e23525a8ce593c310747f049488928fa1286f706
Nathan Herald <me@nathanherald.com>
2025-03-09 21:08:55 +0100

    Remove asset hash for js and css

--- migrate.exs
+++ migrate.exs
@@ -1,31 +1,37 @@
-Path.wildcard("content/**/*.md")
+Path.wildcard("public/**/*.html")
 |> Enum.map(fn path ->
-  url =
-    if String.ends_with?(path, "/index.md") do
-      Path.dirname(path)
-    else
-      String.replace_trailing(path, ".md", "")
-    end
-
-  {path, url}
+  {File.read!(path), path}
 end)
-|> Enum.map(fn {path, url} ->
-  url = String.replace_leading(url, "content", "") <> "/"
-  {path, url}
-end)
-|> Enum.map(fn {path, url} ->
-  {File.read!(path), path, url}
-end)
-|> Enum.map(fn {file, path, url} ->
-  replacement = """
-  aliases = [
-    "#{url}"
-  ]
-  +++
+|> Enum.map(fn {file, path} ->
+  pattern =
+    ~S{<link rel="stylesheet" type="text/css" href="https://nathanherald.com/combined.min.31312963d9b6322f358005bab4cfc1a3a286b0ec67f5df6e22434de51d71c004.css" integrity="sha256-MTEpY9m2Mi81gAW6tM/Bo6KGsOxn9d9uIkNN5R1xwAQ=">}
 
-  """
+  replacement =
+    ~S{<link rel="stylesheet" type="text/css" href="/styles.css">}
 
-  content = String.replace(file, ~r{\+\+\+\n\n}, replacement)
+  content = String.replace(file, pattern, replacement)
+
+  {content, path}
+end)
+|> Enum.map(fn {file, path} ->
+  pattern =
+    ~S{<script async src="https://nathanherald.com/behavior.min.e41a74d07fee42e04abe287c617e80b67998e06c69d3d3c07c6cd91f272bc0bc.js" integrity="sha256-5Bp00H/uQuBKvih8YX6AtnmY4Gxp09PAfGzZHycrwLw="></script>}
+
+  replacement =
+    ~S{<script async src="/behavior.js"></script>}
+
+  content = String.replace(file, pattern, replacement)
+
+  {content, path}
+end)
+|> Enum.map(fn {file, path} ->
+  pattern =
+    ~S{<link rel="alternate" type="application/rss+xml" href="https://nathanherald.com/posts/rss.xml" title="Archive of all the posts on this site on nathanherald.com">}
+
+  replacement =
+    ~S{<link rel="alternate" type="application/rss+xml" href="/rss.xml" title="Feed of all the posts on nathanherald.com">}
+
+  content = String.replace(file, pattern, replacement)
 
   {content, path}
 end)
